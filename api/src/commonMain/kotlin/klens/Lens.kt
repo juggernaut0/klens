9a96f29e638cabcabbf5bf65580161716c739838
Tom Warner <tomwamt@gmail.com>
2022-10-25 09:55:11 -0600

    WIP

--- api/src/commonMain/kotlin/klens/Lens.kt
+++ api/src/commonMain/kotlin/klens/Lens.kt
@@ -1,12 +1,12 @@
 package klens
 
-interface Lens<R: Any, T> {
+interface Lens<R, T> {
     fun get(obj: R): T
     fun set(obj: R, value: T): R
 }
 
 @Suppress("FunctionName")
-fun <R: Any, T> Lens(
+fun <R, T> Lens(
     getter: R.() -> T,
     setter: R.(T) -> R,
 ): Lens<R, T> {
@@ -16,9 +16,20 @@ fun <R: Any, T> Lens(
     }
 }
 
-operator fun <R: Any, T: Any, U> Lens<R, T>.times(other: Lens<T, U>): Lens<R, U> {
+operator fun <R, T, U> Lens<R, T>.times(other: Lens<T, U>): Lens<R, U> {
     return Lens(
         getter = { other.get(get(this)) },
         setter = { set(this, other.set(get(this), it)) }
+    )
+}
+
+fun <T, R> Lens<T, R>.nullable(): Lens<T?, R?> {
+    return Lens(
+        getter = { this?.let { o -> get(o) } },
+        setter = { this?.also { o ->
+            if (it != null) { // TODO this isn't quite right
+                set(o, it)
+            }
+        } }
     )
 }
